24fa2d11ba3b8b6c0b3ab420fc6787c2933f1020
tvcou <tvcou.zyx77@gmail.com>
2015-05-05 10:34:32 +0000

    "make new"

--- app/controllers/sobjects_controller.rb
+++ app/controllers/sobjects_controller.rb
@@ -8,4 +8,12 @@ class SobjectsController < ApplicationController
     @sobject = Sobject.find(params[:id])
   end
 
+  def new
+    @p_sobject = Sobject.find(params[:pid])
+    @sobject = Sobject.new
+    if @p_sobject.id > 0 then
+      @sobject.pid = @p_sobject.id
+    end
+  end
+
 end
